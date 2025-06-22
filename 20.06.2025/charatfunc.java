3e730aee05ae1960d362823466f38c99ddc8ef56
Vjjd007 <168462030+Vjjd007@users.noreply.github.com>
2025-06-22 19:27:45 +0530

    Create charatfunc.java

---
+++ 20.06.2025/charatfunc.java
@@ -0,0 +1,9 @@
+import java.lang.System;
+import java.util.Scanner;
+class user{
+  public static void main(String args[]){
+    Scanner x = new Scanner(System.in);
+    String a = x.nextLine();
+    System.out.println(a.charAt(2));
+  }
+}
